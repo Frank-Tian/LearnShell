d25a79667a52a59db7a13eb4edc46c199d9308fc
zhenzi <tianzhenzi@inke.cn>
2020-11-10 16:48:37 +0800

    Add bg.sh

---
+++ shell_04/bg.sh
@@ -0,0 +1,19 @@
+#！ /bin/sh
+
+# Test job control
+#
+echo "Script Process ID: $$" #
+count=1
+while [ $count -le 10 ]
+do
+    echo "Loop #$count"
+    sleep 10
+    count=$[ $count + 1 ]
+done
+#
+echo "End of script..."
+#
+
+# sh ./request.sh &
+# bg
+# jobs
